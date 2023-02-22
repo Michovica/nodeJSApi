0fc568f56cae381fbae3119dc2d0832100b27363
Michovica <85302040+Michovica@users.noreply.github.com>
2023-02-22 20:40:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', (error) => console.log("connected to database"))
+
+const subscriberRouter = require('./routers/subscribers')
+app.use('/subscribers', subscriberRouter)
+
+app.listen(3000, () => console.log("server started"))
